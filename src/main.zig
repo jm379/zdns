2020c1671189778ccc204cb4fe3199989a4ca5f8
Rodolfo <rodolfocavoss@gmail.com>
2025-05-04 23:05:02 -0300

    Catching errors

--- src/main.zig
+++ src/main.zig
@@ -10,24 +10,24 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var cargs = try args.ArgV.init(allocator);
-    defer cargs.deinit();
-
-    if (!cargs.args.skip())
-        std.debug.panic("Domain name needed\n", .{});
-
-    const domain = try allocator.dupe(u8, cargs.args.next().?);
-    defer allocator.free(domain);
-    std.debug.print("domain: {s}\n", .{domain});
+    var argv = args.ArgV.init(allocator) catch |err| {
+        std.debug.panic("Failed to parse args: {}\n", .{err});
+    };
+    defer argv.deinit();
+    const domain = args.query_name(argv);
+    defer argv.allocator.free(domain);
 
     var pkt = packet.Packet.init(.{ .allocator = allocator, .query_name = domain, .id = 0x1234 }) catch |err| {
         std.debug.panic("Failed to initialize Packet: {}\n", .{err});
     };
-
     defer pkt.deinit();
 
-    var data_pkt = try allocator.alloc(u8, 512);
-    var len = try pkt.serialize(data_pkt);
+    var data_pkt = allocator.alloc(u8, 512) catch |err| {
+        std.debug.panic("Failed allocate memory for the packet: {}\n", .{err});
+    };
+    var len = pkt.serialize(data_pkt) catch |err| {
+        std.debug.panic("Failed to serialize Packet: {}\n", .{err});
+    };
     var client = server.UDPClient.init(.{ .address = "1.1.1.1", .port = 53 }) catch |err| {
         std.debug.panic("Failed to initialize UDP client: {}\n", .{err});
     };
@@ -40,9 +40,13 @@ pub fn main() !void {
     _ = client.write(data_pkt[0..len]) catch |err| {
         std.debug.panic("Failed to send a message to the server: {any}: {}", .{ client.addr, err });
     };
-    len = try client.read(data_pkt);
+    len = client.read(data_pkt) catch |err| {
+        std.debug.panic("Failed to receive a message from the server: {any}: {}", .{ client.addr, err });
+    };
     std.debug.print("received from the server: {x}\n", .{data_pkt[0..len]});
 
-    pkt = try packet.Packet.deserialize(allocator, data_pkt[0..len]);
+    pkt = packet.Packet.deserialize(allocator, data_pkt[0..len]) catch |err| {
+        std.debug.panic("Failed deserialize the received packet from the server: {}", .{err});
+    };
     std.debug.print("deserialized packet: {any}\n", .{pkt});
 }
